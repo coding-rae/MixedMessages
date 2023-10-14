14c4154ece178a95f3e22ae02a17ae0090bf4bf6
coding.rae <raedwards93@gmail.com>
2023-10-14 22:06:52 +0100

    Added first string variable

---
+++ script.js
@@ -0,0 +1,8 @@
+// create strings of affirmations in variables
+var affirmation1 = "You're so amazing"
+// create random generator - collate above strings
+
+
+
+// return and print output
+console.log('Hey you.' + affirmation1)
