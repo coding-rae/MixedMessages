bbf7e872f32698bbadeb8e62912ceb8364227e10
coding.rae <raedwards93@gmail.com>
2023-10-14 22:30:11 +0100

    Added variable containing three lists of strings

--- script.js
+++ script.js
@@ -3,11 +3,12 @@ function generateRandomNum(num){
     return Math.floor(Math.random() * num)
 }
 
-//Variables of random strings
-
-
-// create random generator - collate above strings
-
+//Variables containing lists of strings
+const AffirmationMessages = {
+    affirmationInfo: ['amazing', 'kind', 'special', 'abundant', 'magical'],
+    adviceInfo: ['go out and have some fun', 'stop being so hard on yourself', 'treat yourself', 'call a friend', 'clean your space'],
+    numInfo: ['1','3','9','11','33' ]
+}
 
 
 // return and print output
