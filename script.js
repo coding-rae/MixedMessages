5ee64912cb1586f67967bb6fefd7785e15d79103
coding.rae <raedwards93@gmail.com>
2023-10-14 22:41:01 +0100

    Added iteration over object and used the objects properties to customise the generated message

--- script.js
+++ script.js
@@ -4,10 +4,39 @@ function generateRandomNum(num){
 }
 
 //Variables containing lists of strings
-const AffirmationMessages = {
+const collectiveMessage = {
     affirmationInfo: ['amazing', 'kind', 'special', 'abundant', 'magical'],
     adviceInfo: ['go out and have some fun', 'stop being so hard on yourself', 'treat yourself', 'call a friend', 'clean your space'],
     numInfo: ['1','3','9','11','33' ]
+}
+
+//Variable for individual 
+const personalMessage = []
+
+//Iterate over object
+
+for (let item in collectiveMessage) {
+    let optionId = generateRandomNum(collectiveMessage[item].length)
+
+ // use the object's properties to customize the message being added to personalWisdom  
+
+ switch(item){
+
+    case 'affirmationInfo':
+    personalMessage.push(`You are so "${collectiveMessage[item][optionId]}".`)
+    break
+
+    case 'adviceInfo':
+    personalMessage.push(`You should "${collectiveMessage[item][optionId]}".`)
+    break
+
+    case 'numInfo':
+    personalMessage.push(`Your lucky number today is "${collectiveMessage[item][optionId]}".`)
+    break
+
+    default:
+        personalMessage.push('Sorry, we need more info!')
+ }
 }
 
 
